1681c80b33620a0aa82c5bf2d281599ee49d5267
basheerjava <basheer.shaik987@gmail.com>
2022-01-06 16:06:10 +0530

    new test class is added

--- src/test/java/com/api/downupload/file_exception_controll.java
+++
@@ -1,1 +0,0 @@
-//this is new class for throw the exception
---
+++ src/test/java/com/api/downupload/file_exception_controll_test.java
@@ -0,0 +1,7 @@
+package com.api.downupload;
+
+
+//this is new class for throw the exception
+public class file_exception_controll_test{
+
+}
